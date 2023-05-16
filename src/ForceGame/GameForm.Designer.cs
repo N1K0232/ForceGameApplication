503725e9604ab0acd331eaba9acbe9cf9e3caa3b
N1K0232 <ns.nicolasilvestri@libero.it>
2023-05-16 17:14:08 +0200

    Add GameForm object

---
+++ src/ForceGame/GameForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+
+namespace ForceGame;
+
+public partial class GameForm : Form
+{
+    private IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            if (components != null)
+            {
+                components.Dispose();
+            }
+        }
+
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+        components = new Container();
+
+        CreateGameField();
+
+        SuspendLayout();
+
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(1000, 800);
+        Name = "GameForm";
+        Text = "Force game";
+
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    private void CreateGameField()
+    {
+        for(int i = 0; i < 7; i++)
+        {
+            for(int j = 0; j < 6; j++)
+            {
+                Button fieldButton = new Button();
+                fieldButton.Tag = $"{i}{j}";
+                fieldButton.Text = $"{i}{j}";
+                fieldButton.Width = 80;
+                fieldButton.Height = 80;
+                fieldButton.Top = 80 * j;
+                fieldButton.Left = 80 * i;
+                fieldButton.Visible = true;
+                fieldButton.Enabled = true;
+                fieldButton.Click += new EventHandler(FieldButton_Click);
+
+                Controls.Add(fieldButton);
+            }
+        }
+    }
+}
---
+++ src/ForceGame/GameForm.cs
@@ -0,0 +1,42 @@
+﻿using ForceGame.Core;
+
+namespace ForceGame;
+
+public partial class GameForm : Form
+{
+    private Game _game;
+
+    public GameForm(Game game)
+    {
+        _game = game;
+        InitializeComponent();
+    }
+
+    private void FieldButton_Click(object sender, EventArgs e)
+    {
+        Button clickedButton = (Button)sender;
+        string tag = Convert.ToString(clickedButton.Tag);
+
+        int row = int.Parse(tag.Substring(0, 1));
+        int column = int.Parse(tag.Substring(1, 1));
+        int color = GetColor(clickedButton);
+
+        _game.Play(row, column, color);
+    }
+
+    private int GetColor(Button button)
+    {
+        int turn = _game.Turn;
+
+        if (turn == 1)
+        {
+            button.BackColor = Color.Red;
+            return 1;
+        }
+        else
+        {
+            button.BackColor = Color.Yellow;
+            return 2;
+        }
+    }
+}
