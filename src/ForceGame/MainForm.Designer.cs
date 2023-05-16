40b84f41a33186828aa16adf7ccac55807d0e368
N1K0232 <ns.nicolasilvestri@libero.it>
2023-05-16 16:19:23 +0200

    MainForm design

--- src/ForceGame/MainForm.Designer.cs
+++ src/ForceGame/MainForm.Designer.cs
@@ -1,38 +1,132 @@
-﻿namespace ForceGame;
+﻿using System.ComponentModel;
 
-partial class MainForm
+namespace ForceGame;
+
+public partial class MainForm : Form
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
+    private IContainer components = null;
 
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    private Label lblFirstPlayer;
+    private Label lblSecondPlayer;
+
+    private TextBox txtFirstPlayer;
+    private TextBox txtSecondPlayer;
+
+    private Button playButton;
+
     protected override void Dispose(bool disposing)
     {
-        if (disposing && (components != null))
+        if (disposing)
         {
-            components.Dispose();
+            if (components != null)
+            {
+                components.Dispose();
+                components = null;
+            }
+
+            lblFirstPlayer.Dispose();
+            lblSecondPlayer.Dispose();
+
+            txtFirstPlayer.Dispose();
+            txtSecondPlayer.Dispose();
+
+            playButton.Dispose();
         }
+
         base.Dispose(disposing);
     }
 
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
+        lblFirstPlayer = new Label();
+        lblSecondPlayer = new Label();
 
-    #endregion
+        txtFirstPlayer = new TextBox();
+        txtSecondPlayer = new TextBox();
+
+        playButton = new Button();
+        
+        SuspendLayout();
+        // 
+        // txtFirstPlayer
+        // 
+        txtFirstPlayer.BackColor = Color.White;
+        txtFirstPlayer.BorderStyle = BorderStyle.FixedSingle;
+        txtFirstPlayer.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+        txtFirstPlayer.ForeColor = Color.Black;
+        txtFirstPlayer.Location = new Point(220, 91);
+        txtFirstPlayer.Name = "txtFirstPlayer";
+        txtFirstPlayer.Size = new Size(280, 36);
+        txtFirstPlayer.TabIndex = 0;
+        // 
+        // txtSecondPlayer
+        // 
+        txtSecondPlayer.BackColor = Color.White;
+        txtSecondPlayer.BorderStyle = BorderStyle.FixedSingle;
+        txtSecondPlayer.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+        txtSecondPlayer.ForeColor = Color.Black;
+        txtSecondPlayer.Location = new Point(220, 155);
+        txtSecondPlayer.Name = "txtSecondPlayer";
+        txtSecondPlayer.Size = new Size(280, 36);
+        txtSecondPlayer.TabIndex = 1;
+        // 
+        // playButton
+        // 
+        playButton.FlatAppearance.BorderColor = Color.Blue;
+        playButton.FlatAppearance.BorderSize = 2;
+        playButton.FlatStyle = FlatStyle.Flat;
+        playButton.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+        playButton.ForeColor = Color.Black;
+        playButton.Location = new Point(295, 224);
+        playButton.Name = "playButton";
+        playButton.Size = new Size(131, 61);
+        playButton.TabIndex = 2;
+        playButton.Text = "Play";
+        playButton.UseVisualStyleBackColor = true;
+        playButton.Click += new EventHandler(OnPlayButtonClick);
+        // 
+        // lblFirstPlayer
+        // 
+        lblFirstPlayer.AutoSize = true;
+        lblFirstPlayer.BorderStyle = BorderStyle.FixedSingle;
+        lblFirstPlayer.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+        lblFirstPlayer.ForeColor = Color.Black;
+        lblFirstPlayer.Location = new Point(122, 91);
+        lblFirstPlayer.Name = "lblFirstPlayer";
+        lblFirstPlayer.Size = new Size(92, 32);
+        lblFirstPlayer.TabIndex = 3;
+        lblFirstPlayer.Text = "Player 1";
+        lblFirstPlayer.TextAlign = ContentAlignment.MiddleLeft;
+        // 
+        // lblSecondPlayer
+        // 
+        lblSecondPlayer.AutoSize = true;
+        lblSecondPlayer.BorderStyle = BorderStyle.FixedSingle;
+        lblSecondPlayer.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+        lblSecondPlayer.ForeColor = Color.Black;
+        lblSecondPlayer.Location = new Point(122, 157);
+        lblSecondPlayer.Name = "lblSecondPlayer";
+        lblSecondPlayer.Size = new Size(92, 32);
+        lblSecondPlayer.TabIndex = 4;
+        lblSecondPlayer.Text = "Player 2";
+        lblSecondPlayer.TextAlign = ContentAlignment.MiddleLeft;
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = Color.White;
+        ClientSize = new Size(687, 362);
+        Controls.Add(lblSecondPlayer);
+        Controls.Add(lblFirstPlayer);
+        Controls.Add(playButton);
+        Controls.Add(txtSecondPlayer);
+        Controls.Add(txtFirstPlayer);
+        ForeColor = Color.Black;
+        Name = "MainForm";
+        Text = "Login Form";
+        
+        ResumeLayout(false);
+        PerformLayout();
+    }
 }
--- src/ForceGame/MainForm.cs
+++ src/ForceGame/MainForm.cs
@@ -1,3 +1,5 @@
+using ForceGame.Core;
+
 namespace ForceGame;
 
 public partial class MainForm : Form
@@ -5,5 +7,28 @@ public partial class MainForm : Form
     public MainForm()
     {
         InitializeComponent();
+    }
+
+    private void OnPlayButtonClick(object sender, EventArgs e)
+    {
+        string firstPlayerName = txtFirstPlayer.Text;
+        string secondPlayerName = txtSecondPlayer.Text;
+
+        if (string.IsNullOrWhiteSpace(firstPlayerName))
+        {
+            MessageBox.Show("the first player name is required", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(secondPlayerName))
+        {
+            MessageBox.Show("the second player name is required", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        var firstPlayer = new Player { Name = firstPlayerName };
+        var secondPlayer = new Player { Name = secondPlayerName };
+
+        var game = new Game(firstPlayer, secondPlayer);
     }
 }
